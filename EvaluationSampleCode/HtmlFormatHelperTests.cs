ad339fcd57e5aeeb5120c7c55f82f50cea92dd3f
Clément Bourgeois <clement.bou57@gmail.com>
2025-01-16 16:37:23 +0100

    Push

--- EvaluationSampleCode/HtmlFormatHelperTests.cs
+++ EvaluationSampleCode/HtmlFormatHelperTests.cs
@@ -17,5 +17,21 @@ namespace EvaluationSampleCode
 
             Console.WriteLine(result == "<strong>Hello</strong>" ? "Test passed" : "Test Failed");
         }
+
+        public void GetItalicFormat_ShouldWrapContentInItalicTags()
+        {
+            var helper = new HtmlFormatHelper();
+            var content = "Hello";
+            var result = helper.GetItalicFormat(content);
+
+            Console.WriteLine(result == "<i>World</i>" ? "Test Passed" : "Test Failed");
+        }
+        public void GetRightListFormatted_ShouldWrapContentInListTags()
+        {
+            var helper = new HtmlFormatHelper();
+            var content = "Hello";
+            var result = helper.GetFormattedListElements(content);
+
+        }
     }
 }
